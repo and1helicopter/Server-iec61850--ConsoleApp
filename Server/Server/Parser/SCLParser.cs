30adaabf00f86401e8675da2e883ac41e6eb0aa0
and1helicopter <and1helicopter@mail.ru>
2017-03-31 16:09:38 +0300

    v0a8
    +Парсит до уровня DA, но не подставляет BDA
    Общая структура модели:
    -Model
    --NodeLD
    ---NodeLN
    ----NodeDO
    -----NodeDA (BDA зациклено само на себе т.е. далее NodeDA)
    ------(NodeDA)

--- Server/Server/Parser/SCLParser.cs
+++ Server/Server/Parser/SCLParser.cs
@@ -15,39 +15,264 @@ namespace Server.Parser
             string filePath = @"Config.icd";
             //читаем данные из файла
             XDocument doc = XDocument.Load(filePath);
-
+            
             if (doc.Root != null)
             {
-                IEnumerable<XElement> xElements = doc.Root.Elements().ToList();
+                XNamespace xNamespace = doc.Root.Name.Namespace;
 
-                IEnumerable<XElement> xElemen = (from x in xElements
-                                                 where x.Name.LocalName == "IED"
-                                                 select x).Elements().ToList();
+                XElement xIED = doc.Root.Element(xNamespace + "IED");
 
+                StructModelObj.Model = new StructModelObj.NodeModel(xIED.Attribute("name").Value);
 
-                IEnumerable<XElement> xEl = (from x in xElements.Descendants()
-                                            where x.Name.LocalName == "LDevice"
-                                            select x).Elements().ToList();
+                IEnumerable<XElement> xLD = (from x in doc.Descendants()
+                                             where x.Name.LocalName == "LDevice"
+                                             select x).ToList();
+
+                foreach (var LD in xLD)
+                {
+                    var xAttribute = LD.Attribute("inst");
+                    if (xAttribute != null)
+                    {
+                        StructModelObj.Model.AddListLD(new StructModelObj.NodeLD(xAttribute.Value));
+
+                        IEnumerable<XElement> xLN = LD.Elements().ToList();
+
+                        foreach (var LN in xLN)
+                        { 
+                            var nameLN = LN.Attribute("lnClass").Value + LN.Attribute("inst").Value;
+                            var lnClassLN = LN.Attribute("lnType").Value;
+                            StructModelObj.Model.ListLD.Last().ListLN.Add(new StructModelObj.NodeLN(nameLN, lnClassLN));
+                        }
+                    }
+                }
+                
+                ParseLN(doc);
+
+                ParseDO(doc);
+
+                ParseDA(doc);
+
+                ParseEnum(doc);
+
+                JoinToModel();
             }
+        }
 
+        private static void JoinToModel()
+        {
+            JoinLNtoLD();
+        }
 
-            //var ll = xRoot.Element("SCL");
+        private static void JoinLNtoLD()
+        {
+            foreach (var LD in StructModelObj.Model.ListLD)
+            {
+                AddDO(LD.ListLN);
+            }
+        }
 
-            // var xElement = doc.Element("SCL"); 
-            // var xRoot = doc.Root;
-            // var xElement = xRoot.Element("SCL");
+        private static void AddDO(List<StructModelObj.NodeLN> ListLN)
+        {
+            foreach (var LN in ListLN)
+            {
+                foreach (var TempLN in StructModelObj.ListTempLN)
+                {
+                    if (LN.LnClassLN == TempLN.LnClassLN)
+                    {
+                        foreach (var TempDO in TempLN.ListDO)
+                        {
+                            LN.ListDO.Add(TempDO);
+                            AddDA(LN);
+                        }
+                    }
+                }
+            }
+        }
 
-            //var xNode = xRoot.Nodes().ToList();
-            //var oo = xNode.Select(from x in xNode
-            //                    where (XElement)x.Name == );
-            // var xElement = (XElement)xNode.
-            // var elem = xElement.Nodes().ToList();
+        private static void AddDA(StructModelObj.NodeLN LN)
+        {
+            foreach (var tempDO in StructModelObj.ListTempDO)
+            {
+                if (LN.ListDO.Last().TypeDO == tempDO.TypeDO)
+                {
+                    foreach (var TempDA in tempDO.ListDA)
+                    {
+                        LN.ListDO.Last().ListDA.Add(TempDA);
+                        /*
+                            AddBDA(LN.ListDO.Last().ListDA);
+                        */
+                    }
+                }
+            }
+        }
 
-            //XElement root = xRoot.Element("SCL");
-            //var xElement = xRoot.Element("IED");
-            //doc.Element("LDevice");
-
+        private static void AddBDA(List<StructModelObj.NodeDA> listDa)
+        {
+            
 
         }
+
+        private static void ParseLN(XDocument doc)
+        {
+            IEnumerable<XElement> xLN = (from x in doc.Descendants()
+                                         where x.Name.LocalName == "LNodeType"
+                                         select x).ToList();
+
+            foreach (var LN in xLN)
+            {
+                var xlnClass = LN.Attribute("lnClass");
+                var xid = LN.Attribute("id"); 
+                if (xlnClass != null && xid != null)
+                {
+                    StructModelObj.ListTempLN.Add(new StructModelObj.NodeLN(xlnClass.Value, xid.Value));
+                    IEnumerable<XElement> xDOElements = (from x in LN.Descendants()
+                                                         where x.Name.LocalName == "DO"
+                                                         select x).ToList();
+                    foreach (var DO in xDOElements)
+                    {
+                        var nameDO = DO.Attribute("name").Value;
+                        var typeDO = DO.Attribute("type").Value;
+
+                        StructModelObj.ListTempLN.Last().AddDOToLN(new StructModelObj.NodeDO(nameDO, typeDO));
+                    }
+                }
+            }
+        }
+
+        private static void ParseDO(XDocument doc)
+        {
+            IEnumerable<XElement> xDO = (from x in doc.Descendants()
+                                         where x.Name.LocalName == "DOType"
+                                         select x).ToList();
+
+            foreach (var DO in xDO)
+            {
+                var nameDO = DO.Attribute("id").Value;
+                var typeDO = DO.Attribute("cdc").Value;
+
+                StructModelObj.ListTempDO.Add(new StructModelObj.NodeDO(nameDO, typeDO));
+
+                IEnumerable<XElement> xDAElements = (from x in DO.Descendants()
+                                                     where x.Name.LocalName == "DA"
+                                                     select x).ToList();
+
+                foreach (var DA in xDAElements)
+                {
+                    var nameDA = DA.Attribute("name").Value;
+                    var fcDA = DA.Attribute("fc") != null
+                        ? DA.Attribute("fc").Value
+                        : DA.Parent.Attribute("fc").Value;
+                    var bTypeDA = DA.Attribute("bType").Value;
+                    var typeDA = DA.Attribute("type") != null
+                        ? DA.Attribute("type").Value
+                        : DA.Parent.Attribute("type") != null ? DA.Parent.Attribute("type").Value : null;
+                    var trgOpsDA = TriggerOptions.NONE;
+                    var countDA = DA.Attribute("count") != null ? DA.Attribute("count").Value : "0";
+
+                    if (bTypeDA.Equals("Enum"))
+                    {
+                        bTypeDA = String.Concat(bTypeDA, " (Integer)");
+                    }
+
+                    var dchgDA = DA.Attribute("dchg");
+                    if ((dchgDA != null ? dchgDA.Value : "false").ToLower() == "true")
+                        trgOpsDA |= TriggerOptions.DATA_CHANGED;
+
+                    var qchgDA = DA.Attribute("qchg");
+                    if ((qchgDA != null ? qchgDA.Value : "false").ToLower() == "true")
+                        trgOpsDA |= TriggerOptions.QUALITY_CHANGED;
+
+                    var dupdDA = DA.Attribute("dupd");
+                    if ((dupdDA != null ? dupdDA.Value : "false").ToLower() == "true")
+                        trgOpsDA |= TriggerOptions.DATA_UPDATE;
+
+                    StructModelObj.ListTempDO.Last()
+                        .AddDAToDA(new StructModelObj.NodeDA(nameDA, fcDA, bTypeDA, typeDA, (byte)trgOpsDA, countDA));
+                }
+            }
+        }
+
+        private static void ParseDA(XDocument doc)
+        {
+            IEnumerable<XElement> xDA = (from x in doc.Descendants()
+                                         where x.Name.LocalName == "DAType"
+                                         select x).ToList();
+
+            foreach (var DA in xDA)
+            {
+                var nameDA = DA.Attribute("id").Value;
+
+                StructModelObj.ListTempDA.Add(new StructModelObj.TypeDA(nameDA));
+
+                IEnumerable<XElement> xDAElements = (from x in DA.Descendants()
+                                                     where x.Name.LocalName == "BDA"
+                                                     select x).ToList();
+
+                foreach (var BDA in xDAElements)
+                {
+                    var nameBDA = BDA.Attribute("name").Value;
+                    var fcBDA = BDA.Attribute("fc") != null ? BDA.Attribute("fc").Value : null;
+                    var bTypeBDA = BDA.Attribute("bType").Value;
+                    var typeBDA = BDA.Attribute("type") != null ? BDA.Attribute("type").Value : null;
+                    var trgOpsBDA = TriggerOptions.NONE;
+                    var countBDA = BDA.Attribute("count") != null ? BDA.Attribute("count").Value : "0";
+
+                    if (bTypeBDA.Equals("Enum"))
+                    {
+                        bTypeBDA = String.Concat(bTypeBDA, " (Integer)");
+                    }
+
+                    var dchgBDA = BDA.Attribute("dchg");
+                    if ((dchgBDA != null ? dchgBDA.Value : "false").ToLower() == "true")
+                        trgOpsBDA |= TriggerOptions.DATA_CHANGED;
+
+                    var qchgBDA = BDA.Attribute("qchg");
+                    if ((qchgBDA != null ? qchgBDA.Value : "false").ToLower() == "true")
+                        trgOpsBDA |= TriggerOptions.QUALITY_CHANGED;
+
+                    var dupdBDA = BDA.Attribute("dupd");
+                    if ((dupdBDA != null ? dupdBDA.Value : "false").ToLower() == "true")
+                        trgOpsBDA |= TriggerOptions.DATA_UPDATE;
+
+                    StructModelObj.ListTempDA.Last().ListDA.Add(new StructModelObj.NodeDA(nameBDA, fcBDA, bTypeBDA, typeBDA, (byte)trgOpsBDA, countBDA));
+                }
+            }
+        }
+
+        private static void ParseEnum(XDocument doc)
+        {
+            IEnumerable<XElement> xEnum = (from x in doc.Descendants()
+                                         where x.Name.LocalName == "EnumType"
+                                         select x).ToList();
+
+            foreach (var Enum in xEnum)
+            {
+                var nameEnum = Enum.Attribute("id").Value;
+
+                StructModelObj.ListEnumType.Add(new StructModelObj.EnumType(nameEnum));
+
+                IEnumerable<XElement> xEnumVal = (from x in Enum.Descendants()
+                                                     where x.Name.LocalName == "EnumVal"
+                                                     select x).ToList();
+
+                foreach (var EnumVal in xEnumVal)
+                {
+                    var nameEnumVal = Convert.ToInt32(EnumVal.Attribute("ord").Value);
+                    var valEnumVal = EnumVal.Value != null ? EnumVal.Value : "";
+
+                    StructModelObj.ListEnumType.Last().ListEnumVal.Add(new StructModelObj.EnumType.EnumVal(nameEnumVal, valEnumVal));
+                }
+            }
+        }
+    }
+    
+    public enum TriggerOptions
+    {
+        NONE = 0,
+        DATA_CHANGED = 1,
+        QUALITY_CHANGED = 2,
+        DATA_UPDATE = 4,
+        INTEGRITY = 8,
+        GI = 16
     }
 }
--- Server/Server/Parser/StructDataObj.cs
+++ Server/Server/Parser/StructDataObj.cs
@@ -43,4 +43,140 @@ namespace Server.Parser
             structDataObj.Clear();
         }
     }
+
+    public static class StructModelObj
+    {
+        public static NodeModel Model;
+
+        public class NodeModel
+        {
+            public string NameModel { get; private set; }
+            public List<NodeLD> ListLD= new List<NodeLD>();
+
+            public NodeModel(string nameModel)
+            {
+                NameModel = nameModel;
+            }
+
+            public void AddListLD(NodeLD LD)
+            {
+                ListLD.Add(LD);
+            }
+        }
+        
+        public class NodeLD
+        {
+            public string NameModel { get; private set; }
+            public List<NodeLN> ListLN = new List<NodeLN>();
+
+            public NodeLD(string nameModel)
+            {
+                NameModel = nameModel;
+            }
+        }
+
+        public static List<NodeLN> ListTempLN = new List<NodeLN>();
+
+        public class NodeLN
+        {
+            public string NameLN { get; private set; }
+            public string LnClassLN { get; private set; }
+            public List<NodeDO> ListDO = new List<NodeDO>();
+
+            public NodeLN(string nameLN, string lnClassLN)
+            {
+                NameLN = nameLN;
+                LnClassLN = lnClassLN;
+            }
+
+            public void AddDOToLN(NodeDO DO)
+            {
+                ListDO.Add(DO);
+            }
+        }
+
+        public static List<NodeDO> ListTempDO= new List<NodeDO>();
+
+        public class NodeDO
+        {
+            public string NameDO { get; private set; }
+            public string TypeDO { get; private set; }
+            public List<NodeDA> ListDA = new List<NodeDA>();
+
+            public NodeDO(string nameDO , string typeDO)
+            {
+                NameDO = nameDO;
+                TypeDO = typeDO;
+            }
+
+            public void AddDAToDA(NodeDA DA)
+            {
+                ListDA.Add(DA);
+            }
+        }
+
+
+
+        public class NodeDA
+        {
+            public string NameDA { get; private set; }
+            public string FCDA { get; private set; }
+            public string BTypeDA { get; private set; }
+            public string TypeDA { get; private set; }
+            public byte TrgOpsDA { get; private set; }
+            public string CountDA { get; private set; }
+
+            public List<NodeDA> ListDA = new List<NodeDA>();
+
+            public NodeDA(string nameDA, string fcDA,string bTypeDa, string typeDA, byte trgOpsDA, string countDA)
+            {
+                NameDA = nameDA;
+                FCDA = fcDA;
+                BTypeDA = bTypeDa;
+                TypeDA = typeDA;
+                TrgOpsDA = trgOpsDA;
+                CountDA = countDA;
+            }
+        }
+
+        public static List<TypeDA> ListTempDA = new List<TypeDA>();
+        
+        public class TypeDA
+        {
+            public string NameTypeDA { get; private set; }
+
+            public List<NodeDA> ListDA = new List<NodeDA>();
+
+            public TypeDA(string nameTypeDA)
+            {
+                NameTypeDA = nameTypeDA;
+            }
+        }
+
+        public static List<EnumType> ListEnumType = new List<EnumType>();
+
+        public class EnumType
+        {
+            public string NameEnumType { get; private set; }
+
+            public List<EnumVal> ListEnumVal = new List<EnumVal>();
+
+            public EnumType(string nameEnumType)
+            {
+                NameEnumType = nameEnumType;
+            }
+
+            public class EnumVal
+            {
+                public int OrdEnumVal { get; private set; }
+                public string ValEnumVal { get; private set; }
+
+                public EnumVal(int ordEnumVal, string valEnumVal)
+                {
+                    OrdEnumVal = ordEnumVal;
+                    ValEnumVal = valEnumVal;
+                }
+            }
+        }
+    }
 }
